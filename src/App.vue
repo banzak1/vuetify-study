e10a911487223af647a68d646d34eed3e7444c60
banzak1 <leosantanabr@gmail.com>
2021-12-28 11:46:01 -0300

    preparaçao para criaçao das rotas

--- src/App.vue
+++ src/App.vue
@@ -15,34 +15,13 @@
         {{ link }}
       </v-btn>
     </v-app-bar>
+
     <!-- Login -->
     <v-main>
-      <v-card with="500" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field
-              label="Username"
-              prepend-icon="mdi-account-circle"
-            ></v-text-field>
-            <v-text-field
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="exibir ? 'mdi-eye' : 'mdi-eye-off'"
-              :type="exibir ? 'text' : 'password'"
-              @click:append="showHide"
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <v-router></v-router>
     </v-main>
+
+    <!-- Footer -->
     <v-footer color="purple darken-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
@@ -71,13 +50,6 @@ export default {
 
   data: () => ({
     links: ["Home", "Login", "Cadastro"],
-    exibir: false,
   }),
-
-  methods: {
-    showHide() {
-      this.exibir = !this.exibir;
-    },
-  },
 };
 </script>
